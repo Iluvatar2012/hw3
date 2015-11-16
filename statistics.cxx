2f2582cb3d8baa2b83749a2ed9dd30e4f4aa890d
Aiko Bernehed <aiko.bernehed@hhu.de>
2015-11-16 11:56:59 +0100

    solved assignement with const, references and all sorts of other fancy stuff ;)

--- statistics.cxx
+++ statistics.cxx
@@ -1,21 +1,58 @@
 #include <cmath>
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
 //--------------------------------------------------
 // plenty of lines here
 //--------------------------------------------------
+void fill (double* const p, const int N) {
+
+	// initialize random number generator with current time stamp
+	srand(time(NULL));
+
+	// iterate over all entries
+	for (int i=0; i<N; i++)
+		p[i] = rand()/(1.*RAND_MAX);
+}
+
+
+void statistics (const double* const p, const int N, double& mean, double& var) {
+
+	// reinitialize variables
+	mean = 0;
+	var  = 0;
+
+	// iterate the mean of the distribution
+	for (int i=0; i<N; i++) {
+		mean += p[i];
+	}
+
+	mean /= N;
+
+	// iterate the variance of the distribution
+	for (int i=0; i<N; i++) {
+		var += (p[i] - mean)*(p[i] - mean);
+	}
+
+	var /= N;
+}
+
 
 int main(){
-   const int N = 100;
-   double p[N];
-   double mean, var;
 
-   // Some lines here....
+	const int N = 100;
+	double p[N];
 
-   cout << "Mean = " << mean << endl;
-   cout << "Variance = " << var << endl;
+	double mean, var;
 
-   return 0;
+	// fill the array with random numbers, do statistical shit
+	fill(p, N);
+	statistics(p, N, mean, var);
+
+	cout << "Mean = " << mean << endl;
+	cout << "Variance = " << var << endl;
+
+	return 0;
 }
